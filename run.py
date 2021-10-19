7a7527cae27b99e38e2837ed56f67b2fd88b5fc9
Stephen <sdarcy29@hotmail.com>
2021-10-19 14:57:20 +0000

    Add cost and removed errors from code

--- run.py
+++ run.py
@@ -10,7 +10,7 @@
 
 import time
 
-a = 2
+TIME_ELAPSED = 2
 
 print("""
       pass
@@ -25,20 +25,20 @@ def start():
     print("\n The Great Castle Escape\n")
 
     print("\n You awake a little dazed and confused.")
-    time.sleep(a)
+    time.sleep(TIME_ELAPSED)
     print("\n You can vaguely hear rain crashing down on a tiled roof.")
-    time.sleep(a)
+    time.sleep(TIME_ELAPSED)
     print("\n The sound of thunder rings in the air.")
-    time.sleep(a)
+    time.sleep(TIME_ELAPSED)
     print("\n As your senses come back you try to recall the past few hours, \
 but everything is hazy.")
-    time.sleep(a)
+    time.sleep(TIME_ELAPSED)
     print("\n As you look around your surroundings \
 you appear to be in a room \
 with just a large wooden door and a \
 mall window on one side of the room. \
 The walls are stone and look damp.")
-    time.sleep(a)
+    time.sleep(TIME_ELAPSED)
 
     print("\n So do you have the guts to try and escape? (y or n) ")
     # convert the player's input to lower_case
@@ -49,7 +49,7 @@ The walls are stone and look damp.")
     elif answer == "n":
         # take player to play_again()
         print("\n Shame")
-        time.sleep(a)
+        time.sleep(TIME_ELAPSED)
         print("\n Enjoy the solitude and loniness of the tower")
         play_again()
     else:
@@ -63,9 +63,9 @@ def small_window():
     or use the door
     """
     print("\n You get up and walk towards the window.")
-    time.sleep(a)
+    time.sleep(TIME_ELAPSED)
     print("\n You peer out and can only see darkness")
-    time.sleep(a)
+    time.sleep(TIME_ELAPSED)
     print("\n Will you try open the window")
 
     window = input(">").lower()
@@ -80,6 +80,10 @@ towards the door")
 
 
 def large_door():
+    """
+    User approches the large door in the first room and \
+
+    """
     print("\n You walk towards the door and \
 try the handle, to your surprise it opens")
     print("\n Do you proceed through the door?")
@@ -96,7 +100,27 @@ the door and peer from side to side")
         drawer = input(">").lower()
 
         if drawer == "y":
-            print("\n You pull at the drawr and it opens, inside is a key and a small knife")
+            print("\n You pull at the drawr and it opens, inside is a key and\
+ a small knife")
+        take_items()
+
+    else:
+        print("\n You close the drawer and move on")
+
+
+def take_items():
+    """
+    Options for the user to take the items from the drawer
+    """
+    print("\n You you pick up the items from the drawer? (y or n")
+
+    pick_items = input(">").lower()
+
+    if pick_items == "y":
+        print("\n You reach in and pick up both items and \
+place them in your pockets")
+    else:
+        print("\n You shut the drawer silently")
 
 
 def play_again():
