0a723e49ed69c2d904adc5ee8e155e4b94f65916
Stephen <sdarcy29@hotmail.com>
2021-10-29 18:05:03 +0000

    Small vhange to text spacing

--- run.py
+++ run.py
@@ -48,10 +48,10 @@ def start():
     print("\n As your senses come back you try to recall the past few hours,\
 but everything is hazy.")
     time.sleep(TIME_ELAPSED)
-    print("\n As you look around your surroundings you appear to be in a room with \
-just a large wooden door and a \
-small window on one side of the room. \
-The walls are stone and look damp.")
+    print("\n As you look around your surroundings you appear to be in a room \
+with just") 
+    print("\n a large wooden door and a small window on one side of the room.") 
+    print("\n The walls are stone and look damp.") 
     time.sleep(TIME_ELAPSED)
 
     print("\n So do you have the guts to try and escape? (y or n) ")
